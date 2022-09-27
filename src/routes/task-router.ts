e3c6ffe1b5db75abe7aa975eeee73da4d30fe943
Eddiebee <edbeeotudor@gmail.com>
2022-09-27 11:02:22 +0100

    Add post route

--- src/routes/task-router.ts
+++ src/routes/task-router.ts
@@ -2,9 +2,15 @@ import { Router } from "express";
 
 const router = Router();
 
-router.route("/").get((req, res) => {
-  console.log("Get route");
-  res.send("Get route");
-});
+router
+  .route("/")
+  .get((req, res) => {
+    console.log("Get route");
+    res.send("Get route");
+  })
+  .post((req, res) => {
+    console.log("Post route");
+    res.send("Post route");
+  });
 
 export default router;
