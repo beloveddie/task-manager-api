cf7441d2e1960a31c7ddf275b65b138452a465c9
Eddiebee <edbeeotudor@gmail.com>
2022-09-27 10:37:11 +0100

    Connect to Mongo Database and spin up server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import connectDB from "./helpers/connectToDb";
 
 dotenv.config();
 
@@ -8,6 +9,15 @@ const port = process.env.PORT;
 
 app.use(express.static("./src/views"));
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
-});
+(async () => {
+  // connect to database
+  try {
+    await connectDB(process.env.MONGO_DB_URI!);
+    console.log("Successfully connected to DB");
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at https://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Something went wrong" + error);
+  }
+})();
