3ad36cc90c69eba3551d4c60d91dc4049b0aaa07
Eddiebee <edbeeotudor@gmail.com>
2022-09-28 10:37:43 +0100

    Correct the casing in connectDB import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import connectDB from "./helpers/connectToDb";
+import connectDB from "./helpers/connectToDB";
 import taskRouter from "./routes/task-router";
 
 dotenv.config();
