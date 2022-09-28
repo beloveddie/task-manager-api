7238efded1723a1cacb1e25306f3847674149d69
Eddiebee <edbeeotudor@gmail.com>
2022-09-28 11:30:19 +0100

    Add basic validations to TaskSchema

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,13 +1,25 @@
 import { Schema, model } from "mongoose";
 
 const TaskSchema = new Schema({
-  title: String,
-  assignee: String,
-  startDate: Date,
-  dueDate: Date,
-  todo: Boolean,
-  doing: Boolean,
-  done: Boolean,
+  title: {
+    type: String,
+    required: [true, "Title of a task must be specified."],
+  },
+  assignee: {
+    type: String,
+    required: [true, "Make sure a task is assigned to someone."],
+  },
+  startDate: {
+    type: Date,
+    required: [true, "We need to know when to kick-off."],
+  },
+  dueDate: {
+    type: Date,
+    required: [true, "There should always be an expected end."],
+  },
+  todo: { type: Boolean, default: true },
+  doing: { type: Boolean, default: false },
+  done: { type: Boolean, default: false },
 });
 
 export const Task = model("Task", TaskSchema);
